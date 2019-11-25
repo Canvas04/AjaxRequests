3c485b4ee77abf3baf577b45f1af3652e8c9e23f
Ruslan04071996 <ruslan040796@mail.ru>
2019-11-25 21:15:28 +0300

    рефакторинг app.js

--- app.js
+++ app.js
@@ -1,46 +1,4 @@
 import { Api} from '/lib.js';
 
-const api = new Api('http://localhost:9999/posts');
-{
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', 'http://localhost:9999/posts');
-    xhr.addEventListener('load', ev => {
-        if (xhr.status >= 200 && xhr.status < 300) {
-            const data = JSON.parse(xhr.responseText);
-            console.log(data);
-            return;
-        }
-
-        console.log(xhr.statusText);
-
-    });
-
-    xhr.addEventListener('error', ev => {
-        console.log('Unexpected error');
-    });
-    xhr.send();
-}
-
-{
-    const xhr = new XMLHttpRequest();
-    xhr.open('POST', 'http://localhost:9999/posts');
-    xhr.setRequestHeader('Content-Type' , 'application/json')
-    xhr.addEventListener('load', ev => {
-        if (xhr.status >= 200 && xhr.status < 300) {
-            const data = JSON.parse(xhr.responseText);
-            console.log(data);
-            return;
-        }
-
-        console.log(xhr.statusText);
-
-    });
-
-    xhr.addEventListener('error', ev => {
-        console.log('Unexpected error');
-    });
-    xhr.send(JSON.stringify({
-        id:0,
-        content: 'New Post',
-    }));
-}
+const api = new Api('http://localhost:9999');
+api.getJSON('/posts');
