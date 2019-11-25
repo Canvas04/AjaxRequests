42ff4e96da05e2577ffa50600198089f704876ad
Ruslan04071996 <ruslan040796@mail.ru>
2019-11-25 20:19:20 +0300

    Первый коммит

---
+++ app.js
@@ -0,0 +1,12 @@
+const xhr = new XMLHttpRequest();
+xhr.open('GET' , 'http://localhost:9999/posts');
+xhr.addEventListener('load' , ev => {
+if (xhr.status >= 200 && xhr.status < 300) {
+    const data = JSON.parse(xhr.responseText);
+    console.log(data);
+}
+});
+xhr.addEventListener('error' , ev => {
+console.log(xhr);
+});
+xhr.send();
