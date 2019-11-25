917dce8d64d995d0553eb6e09de16aeea32adc0b
Ruslan04071996 <ruslan040796@mail.ru>
2019-11-25 20:29:36 +0300

    запрос Post

--- app.js
+++ app.js
@@ -1,13 +1,43 @@
-const xhr = new XMLHttpRequest();
-xhr.open('GET' , 'http://localhost:9999/posts');
-xhr.addEventListener('load' , ev => {
-if (xhr.status >= 200 && xhr.status < 300) {
-    const data = JSON.parse(xhr.responseText);
-    console.log(data);
-}
-});
+{
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'http://localhost:9999/posts');
+    xhr.addEventListener('load', ev => {
+        if (xhr.status >= 200 && xhr.status < 300) {
+            const data = JSON.parse(xhr.responseText);
+            console.log(data);
+            return;
+        }
 
-xhr.addEventListener('error' , ev => {
-console.log(xhr);
-});
-xhr.send();
+        console.log(xhr.statusText);
+
+    });
+
+    xhr.addEventListener('error', ev => {
+        console.log('Unexpected error');
+    });
+    xhr.send();
+}
+
+{
+    const xhr = new XMLHttpRequest();
+    xhr.open('POSt', 'http://localhost:9999/posts');
+    xhr.setRequestHeader('Content-Type' , 'application/json')
+    xhr.addEventListener('load', ev => {
+        if (xhr.status >= 200 && xhr.status < 300) {
+            const data = JSON.parse(xhr.responseText);
+            console.log(data);
+            return;
+        }
+
+        console.log(xhr.statusText);
+
+    });
+
+    xhr.addEventListener('error', ev => {
+        console.log('Unexpected error');
+    });
+    xhr.send(JSON.stringify({
+        id:0,
+        content: 'New Post',
+    }));
+}
